841bbbe13502779c402a02efc24736341705e7b3
Lucas de Oliveira <contatelucasdeoliveira@gmail.com>
2026-03-20 10:00:03 -0300

    feat: adiciona maior precisao nas operações de arredondamento

--- app.py
+++ app.py
@@ -14,15 +14,14 @@ def normalizar_matriz_criterios(matriz_decisao, tipo_criterios):
             if maior_ultima_coluna == 0:
                 maior_ultima_coluna = 1.0
             matriz_normalizada[coluna] = matriz_decisao[coluna].apply(
-                lambda x: np.round(x / maior_ultima_coluna, 1)
+                lambda x: np.floor((x / maior_ultima_coluna) * 10 + 0.5) / 10
             )
         elif criterio == "custo":
             menor_primeira_coluna = np.min(valores_coluna[:, 0])
             matriz_normalizada[coluna] = matriz_decisao[coluna].apply(
-                lambda x: np.round(
-                    np.divide(menor_primeira_coluna, x[::-1], out=np.zeros_like(x, dtype=float), where=x[::-1] != 0),
-                    1,
-                )
+                lambda x: np.floor(
+                    np.divide(menor_primeira_coluna, x[::-1], out=np.zeros_like(x, dtype=float), where=x[::-1] != 0) * 10 + 0.5
+                ) / 10
             )
         else:
             raise ValueError(f"Tipo de critério inválido para {coluna}: {criterio}")
